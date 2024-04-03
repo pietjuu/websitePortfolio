865687214f2c5e00b68ba145d99978de3b08e4e6
pietjuu <pieter.Verheye@gmail.com>
2024-04-03 21:54:08 +0200

    added pictures + updated the events html

---
+++ public/assets/js/typingEvents.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const textElement = document.querySelector('.typing-text');
+    const cursorElement = document.querySelector('.cursor');
+
+    const textToType = ["My events"];
+
+    let index = 0;
+    let currentText = '';
+    let letterIndex = 0;
+
+    function type() {
+        if (index < textToType.length) {
+            if (letterIndex < textToType[index].length) {
+                currentText += textToType[index][letterIndex];
+                textElement.textContent = currentText;
+                letterIndex++;
+                setTimeout(type, 100);
+            }
+        }
+    }
+    type();
+});
